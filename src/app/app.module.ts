9acffd88cccd06ad04883767a1ea09452c2d41b9
Yacoub Mesbahi <yacoubmesbahi.professionnel@gmail.com>
2020-07-12 17:39:50 +0200

    initialisation du plugin Firebase, changement du titre de la page news et profil, ajout d'un label avec texte dans la page profil

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,10 +9,34 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { AngularFireModule } from '@angular/fire';
+import { AngularFireDatabaseModule } from '@angular/fire/database';
+import { AngularFireAuthModule } from '@angular/fire/auth';
+import { AngularFireStorageModule } from '@angular/fire/storage';
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyAzIWVV1_jjzdTi11k4bVhDQBf2gXRKb2g",
+  authDomain: "gameproject-db05a.firebaseapp.com",
+  databaseURL: "https://gameproject-db05a.firebaseio.com",
+  projectId: "gameproject-db05a",
+  storageBucket: "gameproject-db05a.appspot.com",
+  messagingSenderId: "742230684697",
+  appId: "1:742230684697:web:92e0f5eb8e2b83b90ee688",
+  measurementId: "G-WYMNYSWKKX"
+};
+
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [
+    BrowserModule, 
+    IonicModule.forRoot(), 
+    AppRoutingModule,
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireDatabaseModule,
+    AngularFireAuthModule,
+    AngularFireStorageModule
+  ],
   providers: [
     StatusBar,
     SplashScreen,
