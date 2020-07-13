6dadb18cbfbcd78e5d042ff9297954252f395ed5
DarkSidane <sidanealp0@gmail.com>
2020-07-13 15:21:05 +0200

    Maintenant, il y a des news et actualités sur l'écran d'accueil. Ils sont défilables et tout, je projette d'améliorer l'esthétique et aussi essayer de les rendre clickable dans un prochain commit.

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient} from '@angular/common/http';
 
 @Component({
   selector: 'app-tab1',
@@ -7,6 +8,16 @@ import { Component } from '@angular/core';
 })
 export class Tab1Page {
 
-  constructor() {}
+  constructor(
+    public http : HttpClient
+  ) {
+    this.getData();
+  }
+  getData(){
+    let url = 'http://newsapi.org/v2/everything?q=apex%20legends&language=fr&from=2020-06-13&sortBy=publishedAt&apiKey=ceb47a2155124c0cb9d11e95e6b094fe';
+    this.http.get(url).subscribe(data => {
+      this.data = data;
+    });
+  }
 
 }
