1ce18bb50fadae456293e5d0b7f078359b7e4ca2
DarkSidane <sidanealp0@gmail.com>
2020-07-14 10:57:59 +0200

    J'ai résolu un petit bug, qui foutait un écran blanc dans les actus

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -16,7 +16,7 @@ export class Tab1Page {
   }
 
   getData(){
-    let url = 'http://newsapi.org/v2/everything?q=apex%20legends%20%20-Ubisoft%20-Androidpit%20-Ninjala%20-store%20-Gamer%20-fixe&language=fr&from=2020-06-13&sortBy=publishedAt&apiKey=ceb47a2155124c0cb9d11e95e6b094fe'
+    let url = 'http://newsapi.org/v2/everything?q=apex%20legends%20%20-Ubisoft%20-Androidpit%20-Ninjala%20-store%20-%20-GTX%20-Ryzen&language=fr&sortBy=publishedAt&apiKey=ceb47a2155124c0cb9d11e95e6b094fe'
     this.http.get(url).subscribe(data => {
       this.data = data;
     });
