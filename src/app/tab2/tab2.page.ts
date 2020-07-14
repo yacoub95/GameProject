2d47115805198c883f200d6c0900759ab67eebd1
Yacoub Mesbahi <yacoubmesbahi.professionnel@gmail.com>
2020-07-15 00:44:17 +0200

    ajout d'un bouton normalement visible que par nous mais ca ne marche pas pour l'instant

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,12 +1,29 @@
 import { Component } from '@angular/core';
 
+import { AngularFireAuth } from '@angular/fire/auth';
+import { ModalController } from '@ionic/angular';
+
+
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss']
 })
 export class Tab2Page {
+  userId: string;
+  boss : boolean;
 
-  constructor() {}
+  constructor(
+    public afAuth: AngularFireAuth,
+    public modalController: ModalController
+  ) {
+    this.afAuth.authState.subscribe(auth => {
+      if (this.userId == '4LW2JBh5J5gPlUK2XsNOQJntGV32') {
+        this.boss = true;
+      } else {
+        this.boss = false;
+      }
+    });
+  }
 
 }
