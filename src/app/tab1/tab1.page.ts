8fe494c92e2b58e19cc6c11754e4cde35c02bbf8
DarkSidane <sidanealp0@gmail.com>
2020-08-27 12:13:35 +0200

    Construction APK et résolution partielle du problème des news

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -33,7 +33,7 @@ export class Tab1Page {
   //}
 
   getData(){
-    let url = 'http://newsapi.org/v2/everything?q=apex%20legends%20%20-Ubisoft%20-Androidpit%20-Ninjala%20-store%20-%20-GTX%20-Ryzen&language=fr&sortBy=publishedAt&apiKey=ceb47a2155124c0cb9d11e95e6b094fe'
+    let url = 'https://newsapi.org/v2/everything?q=apex%20legends%20%20-Ubisoft%20-Androidpit%20-Ninjala%20-store%20-%20-GTX%20-Ryzen&language=fr&sortBy=publishedAt&apiKey=ceb47a2155124c0cb9d11e95e6b094fe'
     this.http.get(url).subscribe(data => {
       this.data = data;
     });
